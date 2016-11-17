059560651f3e36d385c233131d8a1d6d5589b29f
MikaylaDoan <mikayla@Mikaylas-MacBook-Pro.local>
2016-11-17 12:39:23 -0500

    version 0.1.0

---
+++ R/myfunction.R
@@ -0,0 +1,18 @@
+#' Adding Numbers
+#'
+#' The purpose of this function is to find the sum of a vector of numbers.
+#'
+#' @param x Input vector of numbers
+#' @return Value of sum
+#' @examples
+#' mytotal <- total(c(1,2,3,4,5))
+#' mytotal <- total(x)
+#' @export
+#'
+total <- function(x) {
+  sum2 <- 0
+  for (i in 1:length(x)) {
+    sum2 <- sum2 + x[i]
+  }
+  return(sum2)
+}
